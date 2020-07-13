4072bdb55732d5080ed1f546f36edf891e7a7a9c
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-13 17:30:52 -0300

    Added config.js

---
+++ config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    versionConflictMessage: "!!! Version already exists in repository !!!",
+    interpreterTopics: [
+        "python",
+        "nodejs"
+    ],
+    projectClassTopics: [
+        "flask-app",
+        "react-app",
+        "nodejs-app",
+        "cronjob",
+        "library"
+    ],
+    dockerAppTopics: [
+        "flask-app",
+        "nodejs-app",
+        "cronjob"
+    ],
+    webAppTopics: [
+        "react-app"
+    ],
+    envMappings: {
+        de: "dev",
+        rc: "qa"
+    },
+    branchTypeMappings: {
+        dev: [
+            "develop",
+            "legacy"
+        ],
+        qa: [
+            "release",
+            "legacy"
+        ],
+        prod: [
+            "master",
+            "hotfix",
+            "legacy"
+        ]
+    }
+};
--- index.js
+++ index.js
@@ -6,49 +6,8 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const base64 = require('base-64');
 const AWS = require('aws-sdk');
+const config = require('./config.js');
 
-
-const settings = {
-    "versionConflictMessage": "!!! Version already exists in repository !!!",
-    "interpreterTopics": [
-        "python",
-        "nodejs"
-    ],
-    "projectClassTopics": [
-        "flask-app",
-        "react-app",
-        "nodejs-app",
-        "cronjob",
-        "library"
-    ],
-    "dockerAppTopics": [
-        "flask-app",
-        "nodejs-app",
-        "cronjob"
-    ],
-    "webAppTopics": [
-        "react-app"
-    ],
-    "envMappings": {
-        "dev": "dev",
-        "rc": "qa"
-    },
-    "branchTypeMappings": {
-        "dev": [
-            "develop",
-            "legacy"
-        ],
-        "qa": [
-            "release",
-            "legacy"
-        ],
-        "prod": [
-            "master",
-            "hotfix",
-            "legacy"
-        ]
-    }
-};
 
 function validateTopics(topics, subset, title) {
     let matches = [];
@@ -58,24 +17,25 @@ function validateTopics(topics, subset, title) {
     else return matches[0]
 }
 
-function publishEnvironmentArtifact(environmentVars) {
+function pubEnvArtifact(envVars) {
 
     const artifactClient = artifact.create();
 
-    fs.writeFileSync('./environmentVars.json', JSON.stringify(environmentVars, null, 2));
+    fs.writeFileSync('./environmentVars.json', JSON.stringify(envVars, null, 2));
     artifactClient
         .uploadArtifact('environmentVars', ['environmentVars.json'], '.')
         .catch(error => core.setFailed(error));
 
 }
 
+// Get project metadata from execution environment
 const commitMessage = 'commits' in github.context.payload ? github.context.payload.commits[0].message : '';
 const branchType =  process.env.GITHUB_EVENT_TYPE === 'push' ? process.env.GITHUB_REF.split('/')[2] : false;
-
 const projectName = process.env.GITHUB_REPOSITORY.split('/')[1];
 const fullVersion = ini.parse(fs.readFileSync(process.env.GITHUB_WORKSPACE + '/setup.cfg', 'utf-8'))['bumpversion']['current_version'];
 
-let environmentVars = {
+// Create environment vars object
+let envVars = {
     NODIS_PROJECT_NAME: projectName,
     NODIS_FULL_VERSION: fullVersion,
     NODIS_BASE_VERSION: fullVersion.split('-')[0],
@@ -84,6 +44,7 @@ let environmentVars = {
     NODIS_LEGACY: branchType === 'legacy'
 };
 
+// Fetch project topic from GitHub
 let headers = {Authorization: 'token ' + process.env.GITHUB_TOKEN, Accept: "application/vnd.github.mercy-preview+json"};
 fetch(process.env.GITHUB_API_URL + '/repos/' + process.env.GITHUB_REPOSITORY + '/topics', {headers: headers}).then(response => {
 
@@ -92,21 +53,24 @@ fetch(process.env.GITHUB_API_URL + '/repos/' + process.env.GITHUB_REPOSITORY + '
 
 }).then(response => {
 
-    const interpreter = validateTopics(response.names, settings['interpreterTopics'], 'interpreter');
-    const projectClass = validateTopics(response.names, settings['projectClassTopics'], 'class');
+    // Validate project topics
+    const interpreter = validateTopics(response.names, config['interpreterTopics'], 'interpreter');
+    const projectClass = validateTopics(response.names, config['projectClassTopics'], 'class');
 
+    // Set deployment environment and validate source git branch
     if (branchType && projectClass !== 'library') {
 
         const buildPrefix = fullVersion.split('-')[1];
-        environmentVars.NODIS_ENVIRONMENT = buildPrefix === undefined ? 'prod' : settings['envMappings'][buildPrefix.replace(/[0-9]/g, '')];
-        environmentVars.NODIS_ENVIRONMENT === undefined && core.setFailed('Environment is undefined: ' + fullVersion);
+        envVars.NODIS_DEPLOY_ENV = buildPrefix === undefined ? 'prod' : config['envMappings'][buildPrefix.replace(/[0-9]/g, '')];
+        envVars.NODIS_DEPLOY_ENV === undefined && core.setFailed('Environment is undefined: ' + fullVersion);
 
-        if (!settings['branchTypeMappings'][environmentVars.NODIS_ENVIRONMENT].includes(branchType)) {
+        if (!config['branchTypeMappings'][envVars.NODIS_DEPLOY_ENV].includes(branchType)) {
             core.setFailed('!!! Branch mismatch: version '+ fullVersion + ' should not be published on branch ' + branchType + ' !!!')
         }
 
     }
 
+    // Set python library environment vars
     if (projectClass === 'library' && interpreter === 'python') {
 
         let headers = {Authorization: 'Basic '+ base64.encode(process.env.NODIS_PYPI_USER + ':' + process.env.NODIS_PYPI_PASSWORD)};
@@ -117,40 +81,41 @@ fetch(process.env.GITHUB_API_URL + '/repos/' + process.env.GITHUB_REPOSITORY + '
 
         }).then(response => {
 
-            if (fullVersion in response.releases) core.setFailed(settings['versionConflictMessage']);
-            else publishEnvironmentArtifact(environmentVars)
+            fullVersion in response['releases'] ? core.setFailed(config['versionConflictMessage']) : pubEnvArtifact(envVars)
 
         }).catch(error => core.setFailed(error))
 
-    } else if (settings['dockerAppTopics'].includes(projectClass)) {
+    // Set docker application environment vars
+    } else if (config['dockerAppTopics'].includes(projectClass)) {
 
         let headers = {Authorization: 'Basic '+ base64.encode(process.env.NODIS_REGISTRY_USER + ':' + process.env.NODIS_REGISTRY_PASSWORD)};
         fetch('https://' + process.env.NODIS_REGISTRY_HOST + '/v2/' + projectName + '/manifests/' + fullVersion, {headers: headers}).then(response => {
 
-            response.status === 200 && core.setFailed(settings['versionConflictMessage']);
+            response.status === 200 && core.setFailed(config['versionConflictMessage']);
 
-            environmentVars.NODIS_SERVICE_TYPE = projectClass === 'cronjob' ? 'cronjob' : 'deployment';
-            environmentVars.NODIS_CUSTOM_TAG = environmentVars.LEGACY ? 'legacy' : 'latest';
-            environmentVars.NODIS_IMAGE_NAME = process.env.NODIS_REGISTRY_HOST + '/' + projectName;
-            environmentVars.NODIS_SERVICE_NAME = projectName.replace('_', '-');
-            environmentVars.NODIS_CLUSTER_NAME = JSON.parse(process.env.NODIS_CLUSTER_MAPPINGS)[environmentVars.NODIS_ENVIRONMENT];
+            envVars.NODIS_SERVICE_TYPE = projectClass === 'cronjob' ? 'cronjob' : 'deployment';
+            envVars.NODIS_CUSTOM_TAG = envVars.NODIS_LEGACY ? 'legacy' : 'latest';
+            envVars.NODIS_IMAGE_NAME = process.env.NODIS_REGISTRY_HOST + '/' + projectName;
+            envVars.NODIS_SERVICE_NAME = projectName.replace('_', '-');
+            envVars.NODIS_CLUSTER_NAME = JSON.parse(process.env.NODIS_CLUSTER_MAPPINGS)[envVars.NODIS_DEPLOY_ENV];
 
-            publishEnvironmentArtifact(environmentVars)
+            pubEnvArtifact(envVars)
 
         }).catch(error => core.setFailed(error));
 
-    } else if (settings['webAppTopics'].includes(projectClass)) {
+    // Set webapps environment vars
+    } else if (config['webAppTopics'].includes(projectClass)) {
 
         const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-        environmentVars.NODIS_ARTIFACT_FILENAME = projectName + '-' + fullVersion + '.tgz';
-        environmentVars.NODIS_SUBDOMAIN = JSON.parse(fs.readFileSync(process.env.GITHUB_WORKSPACE +  '/package.json', 'utf-8'))['subdomain'];
+        envVars.NODIS_ARTIFACT_FILENAME = projectName + '-' + fullVersion + '.tgz';
+        envVars.NODIS_SUBDOMAIN = JSON.parse(fs.readFileSync(process.env.GITHUB_WORKSPACE +  '/package.json', 'utf-8'))['subdomain'];
 
-        let bucketParam = {Bucket: 'nodis-webapps', Key: projectName + '/' + environmentVars.NODIS_ARTIFACT_FILENAME};
+        let bucketParam = {Bucket: 'nodis-webapps', Key: projectName + '/' + envVars.NODIS_ARTIFACT_FILENAME};
         s3.headObject(bucketParam, function(err, data) {
 
-            if (err) publishEnvironmentArtifact(environmentVars);
-            else core.setFailed(settings['versionConflictMessage'])
+            if (err) pubEnvArtifact(envVars);
+            else core.setFailed(config['versionConflictMessage'])
 
         });
 
