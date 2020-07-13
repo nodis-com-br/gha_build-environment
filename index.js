931ef311319bae0674c814808de5e81d7d0431fe
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-12 22:24:32 -0300

    Fixes branchType generator for pull requests

--- index.js
+++ index.js
@@ -69,10 +69,10 @@ function publishEnvironmentArtifact(environmentVars) {
 
 }
 
-const commitMessage = github.context.payload.commits[0].message;
+const commitMessage = 'commits' in github.context.payload ? github.context.payload.commits[0].message : '';
+const branchType =  process.env.GITHUB_EVENT_TYPE === 'push' ? process.env.GITHUB_REF.split('/')[2] : false;
 
 const projectName = process.env.GITHUB_REPOSITORY.split('/')[1];
-const branchType =  process.env.GITHUB_EVENT_TYPE === 'push' ? process.env.GITHUB_REF.split('/')[2] : false;
 const fullVersion = ini.parse(fs.readFileSync(process.env.GITHUB_WORKSPACE + '/setup.cfg', 'utf-8'))['bumpversion']['current_version'];
 
 let environmentVars = {
